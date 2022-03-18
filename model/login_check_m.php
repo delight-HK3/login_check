a5971ebd5a99a7ab83d44f73958c7bc208bdeb68
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-03-18 10:49:11 +0900

    Update login_check_m.php
    코드에 설명을 추가했습니다.

--- model/login_check_m.php
+++ model/login_check_m.php
@@ -1,13 +1,16 @@
 <?php
 class Login_check_m extends CI_Model {
     public function getrow($uid,$pwd){
-        $sql="select * from member where uid like '$uid' and pwd like '$pwd'";
-		$query = $this->db->query($sql);
-		if( $query->num_rows() > 0 ){ //회원 정보가 있습니다.
-            return $this->db->query($sql)->row();
-		} else { //회원 정보가 없습니다. 
-			return "no";
-		}
+        $sql="select * from member where uid like '$uid' and pwd like '$pwd'"; 
+        // member 테이블에서 uid와 pwd의 내용이 같은 행을 선택합니다.
+	$query = $this->db->query($sql);
+	if( $query->num_rows() > 0 ){ //회원 정보가 있습니다. 
+        	return $this->db->query($sql)->row(); 
+            	// 회원 정보가 있는 경우 해당하는 행을 리턴합니다.
+	} else { //회원 정보가 없습니다. 
+		return "no"; 
+        	// 회원 정보가 없는 경우 문자열 "no"를 리턴합니다.
+	}
     }
 }
 ?>
